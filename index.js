a6db538ab64f1dd2c44c69962d3c90eb85018aaa
javakian <javakian1@gmail.com>
2021-09-22 19:27:06 -0700

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let firstName = "James"
+let lastName = "Avakian"
+let fullName = firstName + lastName
+console.log(fullName)
